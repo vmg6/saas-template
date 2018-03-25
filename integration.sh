565b151adda2fa7ee6db4fbdfb712b39180df4a0
vladimir_matviichenko <v.matviichenko@epom.com>
2018-03-25 22:27:54 +0300

    bash script for integration test

--- integration.sh
+++ integration.sh
@@ -6,7 +6,7 @@ echo " | [+] Service ..."
 pushd services/grizzly-jersey
 
 export $(cat ./../../.env | grep -v ^# | xargs)
-java -jar target/org.swecourse.services.api-18.3.0-SNAPSHOT-jar-with-dependencies.jar
+nohup java -jar target/org.swecourse.services.api-18.3.0-SNAPSHOT-jar-with-dependencies.jar > nohup.out 2> nohup.err < /dev/null &
 
 echo "git fetch ..."
 git url: 'https://github.com/vmg6/saas-fremework-camp.git
