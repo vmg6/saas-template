ff85358476b8fc4b3c63057a847bfdc4bddb9f4f
vladimir_matviichenko <v.matviichenko@epom.com>
2018-03-25 22:47:02 +0300

    bash script for integration test

--- integration.sh
+++ integration.sh
@@ -9,10 +9,10 @@ export $(cat ./../../.env | grep -v ^# | xargs)
 nohup java -jar target/org.swecourse.services.api-18.3.0-SNAPSHOT-jar-with-dependencies.jar > nohup.out 2> nohup.err < /dev/null &
 
 echo "git fetch ..."
-git url: 'https://github.com/vmg6/saas-fremework-camp.git'
+git url: https://github.com/vmg6/saas-fremework-camp.git
 
 echo "Start Integration test ..."
-mvn clean test -Dservers=env1"
+mvn clean test -Dservers=env1
 
 echo "Stop application ..."
 fuser -k 80/tcp
